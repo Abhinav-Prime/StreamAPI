3af678c988946f5e6b32095335b9aeab0dd7c144
Abhinav Prime <abhinavprime835@gmail.com>
2024-06-04 08:45:35 +0530

    add subscription model

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,25 +1,31 @@
 import { Router } from "express";
-import {loginUser ,logoutUser,registerUser,refreshAccessToken } from "../controllers/user.controller.js";
-import {upload} from '../middlewares/multer.middleware.js'
+import {
+  loginUser,
+  logoutUser,
+  registerUser,
+  refreshAccessToken,
+} from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middlewaare.js";
-const userRouter=Router();
+const userRouter = Router();
 
 userRouter.route("/register").post(
-    upload.fields([
-{
-    name:"avatar",
-    maxCount:1
-},
-{
-    name:"coverImage",
-    maxCount:1
-}
-    ]),
-    
-    registerUser)
-    userRouter.route("/login").post(loginUser)
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+
+  registerUser
+);
+userRouter.route("/login").post(loginUser);
 
 //secured routes
-userRouter.route("/logout").post(verifyJWT, logoutUser)
-userRouter.route("/refresh-token").post(refreshAccessToken)
+userRouter.route("/logout").post(verifyJWT, logoutUser);
+userRouter.route("/refresh-token").post(refreshAccessToken);
 export default userRouter;
