0aa3d21479c4feb5ac88273b2240c2b284d9ea64
Abhinav Prime <abhinavprime835@gmail.com>
2024-06-05 23:30:38 +0530

    add new routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,9 +4,17 @@ import {
   logoutUser,
   registerUser,
   refreshAccessToken,
+  changeCurrentPassword,
+  getCurrentUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
+  getUSerChannelProfile,
+  getWatchHistory,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middlewaare.js";
+import { uploadOnCloudinary } from "../utils/cludinary.js";
 const userRouter = Router();
 
 userRouter.route("/register").post(
@@ -28,4 +36,15 @@ userRouter.route("/login").post(loginUser);
 //secured routes
 userRouter.route("/logout").post(verifyJWT, logoutUser);
 userRouter.route("/refresh-token").post(refreshAccessToken);
+userRouter.route("/change-password").post(verifyJWT, changeCurrentPassword);
+userRouter.route("/current-user").get(verifyJWT, getCurrentUser);
+userRouter.route("/update-account").patch(verifyJWT, updateAccountDetails);
+userRouter
+  .route("/avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+userRouter
+  .route("/cover-image")
+  .patch(verifyJWT, upload.single("/coverImage"), updateUserCoverImage);
+userRouter.route("/c/:username").get(verifyJWT, getUSerChannelProfile);
+userRouter.route("/history").get(verifyJWT, getWatchHistory);
 export default userRouter;
